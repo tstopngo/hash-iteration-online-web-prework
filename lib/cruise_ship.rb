70ede9decb247431f608182e268748801c25f7da
Tracy Ngo <tngo1206@gmail.com>
2019-02-15 01:32:51 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,8 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  passengers.each do |suite, value|
+  passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
